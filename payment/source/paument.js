22353f02406939fbb9983e069cdd35ab49b51584
iphilka <i376765616@yandex.ru>
2024-08-17 02:51:38 +0500

    add payment page

--- payment/source/paument.js
+++ payment/source/paument.js
@@ -4,16 +4,16 @@ const tbank_btn = document.querySelector('.btn_c-tbank');
 sber_btn.addEventListener("click", (e) => {
     e.preventDefault();
     setTimeout(() => {
-        window.location = 'location';
+        window.location = "sbol://p2p/perevod?recipientNumber=9221714929"; 
     }, 5);
-    window.location = "sbol://p2p/perevod?recipientNumber=9221714929";    
+       
 })
 
 tbank_btn.addEventListener("click", (e) => {
     e.preventDefault();
     setTimeout(function() {
-        window.location = 'location';
+        window.location = "tinkoffbank://P2PTransfer?contractId=5171423667";
     }, 5)
-    window.location.href = "tinkoffbank://P2PTransfer?contractId=5171423667";
+    
     
 })
