4e3a1c938178aad95f931aa8e2a4707defdbc2ae
iphilka <i376765616@yandex.ru>
2024-08-17 03:24:31 +0500

    add payment page

--- payment/source/paument.js
+++ payment/source/paument.js
@@ -1,18 +1,18 @@
-const sber_btn = document.querySelector('.btn_c-sber');
-const tbank_btn = document.querySelector('.btn_c-tbank');
+// const sber_btn = document.querySelector('.btn_c-sber');
+// const tbank_btn = document.querySelector('.btn_c-tbank');
 
-window.onload = () => {
-    sber_btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        setTimeout(() => {
-            window.location.href = "sbol://p2p/perevod?recipientNumber=9221714929"; 
-        }, 5);       
-    })
+// window.onload = () => {
+//     sber_btn.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         setTimeout(() => {
+//             window.location.href = "sbol://p2p/perevod?recipientNumber=9221714929"; 
+//         }, 5);       
+//     })
     
-    tbank_btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        setTimeout(function() {
-            window.location.href = "tinkoffbank://P2PTransfer?contractId=5171423667";
-        }, 5);    
-    })
-};
+//     tbank_btn.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         setTimeout(function() {
+//             window.location.href = "tinkoffbank://P2PTransfer?contractId=5171423667";
+//         }, 5);    
+//     })
+// };
