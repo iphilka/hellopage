a03f863916269462f5957d00d5610e725a36e74b
fs.lugovoy <fs.lugovoy@tensor.ru>
2025-07-22 13:18:45 +0500

    Auto calculate age

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ button.addEventListener("click", function (){
     menu.classList.toggle("close");
     menuList.classList.toggle("main-nav--closed");
 });
+
+
+function calculateAge(birthDate) {
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const hasHadBirthdayThisYear =
+    today.getMonth() > birthDate.getMonth() ||
+    (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+  if (!hasHadBirthdayThisYear) {
+    age--;
+  }
+  return age;
+}
+
+const age_field = document.querySelector(".happy-br");
+const birthDate = new Date(1991, 9, 14);
+age_field.textContent = calculateAge(birthDate)
